294c019c4a6b04a4390abe742ea33e15b4a33af5
RudGray <rud.gray@gmail.com>
2023-08-14 15:10:44 +0200

    ajout des variables d'environnement, pour la sécurité des codes et modif ordre route/middleware

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const Parse = require('parse/node');
 const cors = require('cors');
 const app = express();
 
-// Middleware
+// Middleware CORS
 app.use(cors({
   origin: function (origin, callback) {
     console.log('cors')
     console.log('origin', origin);
 
     const allowedOrigins = ['http://localhost:8080', 'https://arkpwa.herokuapp.com'];
-    if (!origin) return callback(null, true);
+    if (!origin) return callback(new Error('Origin is undefined'), false);
     if (allowedOrigins.indexOf(origin) === -1) {
       console.error(`Origin "${origin}" is not allowed by CORS.`);
       return callback(new Error('Origin not allowed by CORS'), false);
@@ -19,9 +19,6 @@ app.use(cors({
     return callback(null, true);
   }
 }));
-
-// Paramètres CORS pour autoriser les requêtes depuis n'importe quelle origine en mode développement
-// app.use(cors());
 
 // Vérification des entêtes avec middleware
 app.use((req, res, next) => {
@@ -31,57 +28,50 @@ app.use((req, res, next) => {
   next();
 });
 
-//Informe le server de la localisation des fichiers statiques
-app.use(express.static('public'));
-// app.use('/public', express.static(path.join(__dirname, 'public')));
-// Serve the private files
-app.use('/private', express.static(path.join(__dirname, 'private')));
-// app.use(express.static('style'));
-app.use('/style', express.static('style'));
-
-//back4app parse server
-Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
-// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
-Parse.initialize(
-  'T77ONDMADOUDe5HzLEdYAdIcl0VRTclFlbhQVXS7', // This is your Application ID
-  'r48cVgYk1O5821fIkxmW1E0YF33Rnq8vPCWp9iJo', // This is your Javascript key
-  'ZNkKDp7wurOXUSTEccDwGpZvrE0cqfEsQE1YOct1' // This is your Master key (never use it in the frontend)
-);
-
-
-
-
+// Route principale
 app.get('/', (req, res) => {
   console.log("route /");
   console.log(__dirname);
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Serveur de fichiers statiques
+app.use(express.static('public'));
+app.use('/private', express.static(path.join(__dirname, 'private')));
+app.use('/style', express.static('style'));
+
+// Configuration de Parse
+Parse.serverURL = 'https://parseapi.back4app.com';
+
+Parse.initialize(
+  process.env.PARSE_APPLICATION_ID,
+  process.env.PARSE_JS_KEY,
+  process.env.PARSE_MASTER_KEY
+);
+
 app.post('/register', async (req, res) => {
-  console.log('register')
-  const {password, email} = req.body;
-  
-  // Use Parse to register the user
+  console.log('register');
+  const { password, email } = req.body;
+
   let user = new Parse.User();
   user.set("password", password);
   user.set("email", email);
 
-  console.log('user', user) 
+  console.log('user', user);
 
   try {
-      await user.signUp();
-      res.status(200).send({ success: true });
-      console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+    await user.signUp();
+    res.status(200).send({ success: true });
+    console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
   } catch (error) {
-      res.status(400).send({ success: false, message: error.message });
-      console.log("Error: " + error.code + " " + error.message);
+    res.status(400).send({ success: false, message: error.message });
+    console.log("Error: " + error.code + " " + error.message);
   }
 });
 
 // Serve the private files only to authenticated users
 app.get('/private/*', function(req, res, next) {
-  const user = Parse.User.current(); // or your own method of getting the user
-  
+  const user = Parse.User.current();
   if (user) {
     next();
   } else {
@@ -89,10 +79,7 @@ app.get('/private/*', function(req, res, next) {
   }
 });
 
-
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
